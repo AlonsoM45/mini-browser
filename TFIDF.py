ee4f99b67e5da3e27286047899b72ccdc3ad453f
Alonso Montero <alonso.monterom61@gmail.com>
2018-05-16 20:18:00 -0600

    Index & Experimental TFIDF
    Document indexation and experimental use of tfidf.

--- TFIDF.py
+++ TFIDF.py
@@ -1,7 +1,33 @@
 import math
+import CosineSimilarity as cs
 import operator
+import numpy as np
 from textblob import TextBlob as tb
 
+def newIndex(documents):
+    indexTF  = []
+    indexIDF = {}
+    for doc in documents:
+        newIndex = newDocumentIndex(doc)
+        indexTF.append(newIndex)
+        for k in newIndex.keys():
+            addWord(indexIDF, k)
+    return indexTF, indexIDF
+
+    
+def newDocumentIndex(document):
+    words = document.split(" ")
+    index = {}
+    for word in words:
+        addWord(index, word)
+    return index
+
+
+def addWord(index, word):
+    try:
+        index[word] = index[word] + 1
+    except:
+        index[word] = 1
 
 def tf(word, blob):
     return blob.words.count(word) / len(blob.words)
@@ -12,29 +38,32 @@ def n_containing(word, bloblist):
 def idf(word, bloblist):
     return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
 
-def tfidf(word, blob, bloblist):
+def tfidf(TF, IDF, word):
     return tf(word, blob) * idf(word, bloblist)
 
+def similarity(TF, IDF, words):
+    vector = []
+    for w in words:
+        try:
+            valueTF = TF[w]
+        except:
+            valueTF = 0
+        try:
+            valueIDF = IDF[w] +1
+        except:
+            valueIDF = 1
+        vector.append(valueTF*valueIDF)
+    #vectorB = np.array(list(map(lambda x: TF[x] * IDF[x], words)))
+    print(vector)
+    return vector
 
-
-
-def searchIFIDF(documents, query):
-    similarity = {}
-    bloblist =[]
-    bloblist += tbDocuments(documents)
+def searchTFIDF(query, documents):
+    TF, IDF = newIndex(documents)
+    vectors = {}
     words = query.split(" ")
-    for i in range(len(bloblist)):
-        v =  list(map( lambda x: tfidf(x, bloblist[i], bloblist), words))
-        #arreglar
-        similarity[i] = sum(v)
-    sortedList = sorted(similarity.items(), key=operator.itemgetter(1), reverse=True)
+    for i in range(len(documents)):
+        vectors[i] = similarity(TF[i],IDF, words)
+    print(vectors)
+    sortedList = sorted(vectors.items(), key=operator.itemgetter(1), reverse=True)
+    print(sortedList)
     return sortedList
-    
-def tbDocuments(documents):
-    bloblist = []
-    for doc in documents:
-        #arreglar
-        bloblist += [tb(doc[0])]
-    return bloblist
-
-
