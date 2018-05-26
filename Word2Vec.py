a4c08b3499aaf4630d161ad1b485e81f5c6a6a82
Andrés Obando Alfaro <31627028+Andres49815@users.noreply.github.com>
2018-05-26 17:08:10 -0600

    x

--- Word2Vec.py
+++ Word2Vec.py
@@ -6,8 +6,8 @@ import csv
 
 FILE_PATH = 'C:\\Users\\Lenovo\\Desktop\\embeddingscsv.csv'
 SHEET = 'embeddings'
-
-words = pd.read_table(FILE_PATH, sep=' ', index_col=0,header=None,quoting=csv.QUOTE_NONE,na_values=None,keep_default_na=False)
+### Manual
+words = pd.read_table(FILE_PATH, sep='\n', index_col=0,header=None,quoting=csv.QUOTE_NONE,na_values=None,keep_default_na=False)
 
 def vec(w):
     return words.loc[w].as_matrix()
@@ -19,3 +19,5 @@ def find_closest_word(v):
     delta = np.sum(diff * diff, axis=1)
     i = np.argmin(delta)
     return words.iloc[i].name
+
+print(words.loc[5])
