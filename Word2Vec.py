998097c5fcf7682bf346c6b79613e4219ac9f460
Alonso Montero <alonso.monterom61@gmail.com>
2018-05-26 21:37:09 -0600

    Revert "W2V Dictionary"
    This reverts commit 046eafbdd24bde2f438e029d1d7583548d14df0e.

--- Word2Vec.py
+++ Word2Vec.py
@@ -1,18 +1,18 @@
 #from openpyxl import load_workbook
 
 import pandas as pd
-import numpy as np
+import numpy 
 import csv
+import pickle
 
-FILE_PATH = 'C:\\Users\\Lenovo\\Desktop\\embeddingscsv.csv'
+FILE_PATH = 'C:\\Users\\Lenovo\\Desktop\\embeddings.csv'
 SHEET = 'embeddings'
 ### Manual
-words = pd.read_table(FILE_PATH, sep='\n', index_col=0,header=None,quoting=csv.QUOTE_NONE,na_values=None,keep_default_na=False)
-
+words = pd.read_csv(FILE_PATH, sep =',', delimiter = '\n')
 def vec(w):
     return words.loc[w].as_matrix()
 
-words_matrix = words.as_matrix()
+words_matrix = words.values
 
 def find_closest_word(v):
     diff = words_matrix - v
@@ -20,4 +20,19 @@ def find_closest_word(v):
     i = np.argmin(delta)
     return words.iloc[i].name
 
-print(words.loc[5])
+def W2V(data):
+    size = len(data)
+    vectorialSpace = {}
+    for i in range(size):
+        fullVector = data.iloc[i].values[0].split(",")
+        try:
+            vectorialSpace[fullVector[0]] = numpy.array(list(map(float,fullVector[1:])))
+        except:
+            print(fullVector)
+    fp = open("W2V.pickle", "wb")
+    pickle.dump(vectorialSpace, fp)
+    fp.close()
+
+
+        
+    
